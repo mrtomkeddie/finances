e258931d9b9e95f09c69506cab9b81eb9581f61f
Tom Keddie <thomaskeddie@gmail.com>
2025-07-09 20:39:36 +0000

    can tapping the logo take me back to the dashboard please?

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -90,7 +90,9 @@ function Header() {
   return (
     <header className="sticky top-0 z-10 border-b bg-background/80 backdrop-blur-lg">
       <div className="mx-auto flex h-20 max-w-screen-2xl items-center justify-between gap-4 px-4 sm:px-6 lg:px-8">
-        <Logo />
+        <Link href="/dashboard">
+          <Logo />
+        </Link>
         <div className="flex items-center gap-2">
           <ThemeToggle />
           <Sheet>
